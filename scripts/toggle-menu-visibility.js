680e4f9b105749c6a4b2a52686cc64699f91b761
DarrensArtist <darrensartist@gmail.com>
2024-10-22 18:12:50 +0100

    changed the hamburger menu so it no longer has social links and removed the bottom border for the responsive design

--- scripts/toggle-menu-visibility.js
+++ scripts/toggle-menu-visibility.js
@@ -1,11 +1,9 @@
 // Hamburger menu toggle
 const hamburger = document.getElementById('hamburger-btn');
 const navigationMain = document.querySelector('.navigation-main'); // Your main nav
-const navigationSocial = document.querySelector('.navigation-social'); // Your social nav
 const navigationContent = document.querySelector('.navigation-content'); // Content nav
 
 hamburger.addEventListener('click', function () {
     navigationMain.classList.toggle('active');
-    navigationSocial.classList.toggle('active');
     navigationContent.classList.toggle('active');
 });
